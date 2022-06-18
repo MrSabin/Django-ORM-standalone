710f30897da43a6cad039c5a1c2b64e257452183
MrSabin <sabin.21011986@gmail.com>
2022-06-18 23:08:00 +0300

    Changed working db to Visit

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Visit  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    passcards = Passcard.objects.all()
-    active_passcards = Passcard.objects.filter(is_active=True)
-    print("Активных пропусков: ", len(active_passcards))
+    visits = Visit.objects.all()
+    print(visits)
